f6749201cc8ab10531f47b13180e8e2c2862cd91
jess-bennett <jessica_bennett@gmx.com>
2020-07-08 12:44:35 +0000

    Remaining JS for play page added
    As before, this has been taken from aborted project and added here.
    
    Functionality testing before each new commit.

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,3 +1,26 @@
+/* Loading div */
+/* https://smallenvelop.com/display-loading-icon-page-loads-completely/ */
+// Wait for window load
+$(window).on('load', function () {
+    $(".se-pre-con").fadeOut("slow");
+    var queryString = location.search.substring(1);
+    
+    if(queryString.length >= 1) { //Checks whether url has substring assigned by learn page
+        var lettersToPlay = queryString.split(",");
+        initialisePlay(lettersToPlay);
+    };
+});
+
+/* On page load */
+$(document).ready(function () {
+    $("#play--display").css("display", "none");
+    $("#play--continue").css("display", "none");
+    $("#play--options__alert").hide();
+    $("#play--all").prop("checked", true);
+    $(".play--letter__select").prop("checked", true);
+    $("#play--options__four").prop("checked", true);
+});
+
 /* Main game object */
 var playLetterBee = {
 
@@ -154,4 +177,273 @@ var playLetterBee = {
             $("#play--word3__display").hide();
         };
     }
+}
+
+/* Settings input */
+$("#play--all").click(function () { //Selects the full 26 letter set
+    if ($("#play--all").prop("checked")) {
+        $(".play--letter__select").prop("checked", true);
+        playLetterBee.resetPlayAlphaBetSet();
+    } else {
+        $(".play--letter__select").prop("checked", false)
+        playLetterBee.playAlphabetSet = [];
+        playLetterBee.updateLettersRemaining();
+    };
+})
+
+$(".play--letter__select").click(function () { //Removes unwanted letters from the play set
+    let checkedLetter = $(this).val();
+    if ($(".play--letter__select:checked").length == $(".play--letter__select").length) {
+        $("#play--all").prop("checked", true);
+    } else {
+        $("#play--all").prop("checked", false);
+    };
+    if ($.inArray(checkedLetter, playLetterBee.playAlphabetSet) > -1) {
+        let removeLetterPerm = playLetterBee.playAlphabetSet.indexOf(checkedLetter);
+        playLetterBee.playAlphabetSet.splice(removeLetterPerm, 1);
+        playLetterBee.updateLettersRemaining();
+        $(this).prop("checked", false);
+    } else {
+        playLetterBee.playAlphabetSet.push(checkedLetter);
+        playLetterBee.playAlphabetSet.sort();
+        playLetterBee.updateLettersRemaining();
+        $(this).prop("checked", true);
+    };
+})
+
+function amendLetterInput(learntArrayFromLearn) { //Amends letter array using values brought over from play page
+    if(learntArrayFromLearn.length >=1) {
+        if(learntArrayFromLearn.length == 26) {
+            $("#play--all").prop("checked", true);
+        } else {
+            playLetterBee.playAlphabetSet = [];
+            $("#play--all").prop("checked", false);
+            $(".play--letter__select").each(function () {
+                if ($.inArray($(this).val(), learntArrayFromLearn) >= 0) {
+                    $(this).prop("checked", true);
+                } else {
+                    $(this).prop("checked", false);
+                };
+            });
+        };
+    };
+    window.history.replaceState({}, document.title, "/" + "play.html"); //Removes substring from URL
+}
+
+function checkLetterInput() { //Recheck checked letters for 'play again'
+    if ($("#play--all").checked) {
+        playLetterBee.resetPlayAlphaBetSet();
+    } else {
+        $(".play--letter__select:checked").each(function () {
+            if ($.inArray($(this).val(), playLetterBee.playAlphabetSet) == -1) {
+                playLetterBee.playAlphabetSet.push($(this).val());
+                playLetterBee.playAlphabetSet.sort();
+                playLetterBee.updateLettersRemaining();
+            };
+        });
+    }
+    if ($(".play--letter__select:checked").length == 0) {
+        $("#play--options__alert").show();
+        return "EXIT FUNCTION";
+    };
+
+}
+
+$(".play--options__select").click(function () { //Updates the number of options to be shown
+    playLetterBee.playNumberOfOptions = parseInt($(this).val());
+    playLetterBee.playImagesValues(playLetterBee.letterToDisplay, playLetterBee.playAlphabetDistractors);
+})
+
+$('audio').prop("muted", false)
+
+$("#play--options__audio").click(function () {
+    $(".play--audio").toggleClass("play--audio__on");
+    $(".play--audio").toggleClass("play--audio__off");
+    if ($('audio').prop("muted")) {
+        $('audio').prop("muted", false);
+    } else {
+        $('audio').prop("muted", true);
+    };
+})
+
+/* https://www.w3schools.com/howto/howto_js_fullscreen.asp */
+
+
+/* When the openFullscreen() function is executed, open the video in fullscreen.
+Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
+function openFullscreen() {
+    var elem = document.getElementById("play--display");
+    $("#play--options__button").css("visibility", "hidden");
+    $("#expand--fullscreen").children("i").removeClass("fas fa-expand-alt").addClass("fas fa-compress-alt");
+    $("#expand--fullscreen").attr("onclick", "closeFullscreen()");
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    $("#play--options__button").css("visibility", "visible");
+    $("#expand--fullscreen").children("i").removeClass("fas fa-compress-alt").addClass("fas fa-expand-alt");
+    $("#expand--fullscreen").attr("onclick", "openFullscreen()");
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
+}
+
+/* Audio function */
+
+function playLettersAudio() {
+
+    let audioFile = masterPlayData.lettersMaster[`letter${playLetterBee.letterToDisplay}`].audioFile;
+    let audioSource = `assets/audio/${audioFile}`;
+    $("#play--letter__audio").attr("src", audioSource);
+
+    setTimeout(function () {
+        $('audio#play--letter__audio')[0].play();
+
+    }, 1500)
+
+}
+
+function playIncorrectAudio() {
+    let audioMain = masterPlayData.lettersMaster[`letter${playLetterBee.letterToDisplay}`].audioFile;
+    let audioVoice = audioMain.substr(9, 1);
+    let audioSource = `assets/audio/have-another-go-${audioVoice}.mp3`;
+    $("#play--incorrect__audio").attr("src", audioSource);
+
+    $('audio#play--incorrect__audio')[0].play();
+}
+
+function playCorrectAudio() {
+    let audioMain = masterPlayData.lettersMaster[`letter${playLetterBee.letterToDisplay}`].audioFile;
+    let audioVoice = audioMain.substr(9, 1);
+    let audioSourceCorrect = `assets/audio/thats-right-${audioVoice}.mp3`;
+    let wordSyntax = masterPlayData.lettersMaster[`letter${playLetterBee.letterToDisplay}`].matchingWords[playLetterBee.wordVar1].wordSyntax;
+    let amendedWordSyntax = wordSyntax.substr(0, wordSyntax.indexOf('.'));
+    let audioSourceConfirmation = `assets/audio/${amendedWordSyntax}-long-${audioVoice}.mp3`;
+
+    $("#play--correct__audio").attr("src", audioSourceCorrect);
+    $("#play--confirmation__audio").attr("src", audioSourceConfirmation);
+
+    $('audio#play--correct__audio')[0].play();
+
+    $('audio#play--correct__audio').on('ended', function () {
+        $('audio#play--confirmation__audio')[0].play();
+    });
+    $('audio#play--confirmation__audio').on('ended', function () {
+        $('audio#play--confirmation__audio').off('ended');
+        resetDisplay();
+        playLetterBee.checkPlayProgress();
+        $(".play--image__select").click(function () { // To turn click event back on
+            let imageID = this.id.match(/\d+/)[0];
+            $(`#play--word${imageID}__display`).css("visibility", "visible");
+            $(`#play--answer`).css("visibility", "visible");
+            let imageValue = $(this).children("img").attr("alt").toUpperCase();
+            if (imageValue.startsWith(playLetterBee.letterToDisplay)) {
+                correctSelected(this);
+            } else {
+                incorrectSelected(this);
+            };
+        })
+    });
+}
+
+/* Reset function */
+function resetDisplay() {
+    $(".play--image__select").removeClass("play--image__correct play--image__incorrect");
+    $("#play--answer").css("visibility", "hidden").removeClass("play--image__incorrect");
+    $(".play--word").css("visibility", "hidden");
+}
+
+/* Initialise game */
+function initialisePlay(learntArrayFromLearn) {
+    if (typeof learntArrayFromLearn !== 'undefined') {
+        amendLetterInput(learntArrayFromLearn);
+    };
+    if(checkLetterInput() == "EXIT FUNCTION") {
+        return;
+    } else {   
+    $("#play--options__alert").hide();    
+    $("#play--options").modal("hide");    
+    playLetterBee.resetPlayArrays();
+    resetDisplay();
+    $("#page--welcome").css("display", "none");
+    $("#play--display").css("display", "block");
+    $("#play--continue").css("display", "block");
+    $("#play--restart").text("Restart game");
+    nextPlay();
+    };
+}
+
+function playAgain() {
+    $("#play--continue").css("display", "none");
+    $("#play--restart").text("Play game");
+}
+
+function nextPlay() {
+
+    let keyLetter = playLetterBee.randomiseKeyLetter();
+    playLetterBee.updateLettersRemaining()
+        .removeLetterPerm(keyLetter)
+        .createAlphabetDistractors(keyLetter)
+        .createWordIndex()
+        .playImagesValues(keyLetter, playLetterBee.playAlphabetDistractors);
+
+    playLettersAudio();
+}
+
+/* Select answer */
+$(".play--image__select").click(function () {
+    let imageID = this.id.match(/\d+/)[0];
+    $(`#play--word${imageID}__display`).css("visibility", "visible");
+    $(`#play--answer`).css("visibility", "visible");
+    let imageValue = $(this).children("img").attr("alt").toUpperCase();
+    if (imageValue.startsWith(playLetterBee.letterToDisplay)) {
+        correctSelected(this);
+    } else {
+        incorrectSelected(this);
+    };
+})
+
+
+
+function incorrectSelected(selectedImage) {
+    $(selectedImage).addClass("play--image__incorrect");
+    $("#play--answer").text("Have another go!")
+    $("#play--answer").addClass("play--image__incorrect");
+    playIncorrectAudio();
+    if ($.inArray(playLetterBee.letterToDisplay, playLetterBee.playIncorrectArray) == -1) {
+        playLetterBee.playIncorrectArray.push(playLetterBee.letterToDisplay);
+        playLetterBee.playIncorrectArray.sort();
+    };
+}
+
+function correctSelected(selectedImage) {
+    $(selectedImage).addClass("play--image__correct");
+    $("#play--answer").text(`That's right! ${playLetterBee.letterToDisplay} is for ${playLetterBee.correctAnswer}`);
+    $("#play--answer").removeClass("play--image__incorrect");
+    $("#play--answer").addClass("play--image__correct");
+    $(".play--image__select").off("click");
+    playCorrectAudio();
+    if ($.inArray(playLetterBee.letterToDisplay, playLetterBee.playIncorrectArray) == -1) {
+        playLetterBee.playCorrectArray.push(playLetterBee.letterToDisplay);
+        playLetterBee.playCorrectArray.sort();
+    };
+
+}
+
+function goToLearnMode() {
+    window.location.href = `learn.html?${playLetterBee.playIncorrectArray}`;
 }

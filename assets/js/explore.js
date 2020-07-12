6c9ce7afbba7183e48c81a5577a95f5ff2e56a34
jess-bennett <jessica_bennett@gmx.com>
2020-07-12 11:07:50 +0000

    explore.js file added

---
+++ assets/js/explore.js
@@ -0,0 +1,9 @@
+/* Loading div */
+$(window).on('load', function() {
+  $(".se-pre-con").fadeOut("slow");
+});
+
+/* On page load */
+$(document).ready(function() {
+  $("#explore--answer").css("display", "none");
+});

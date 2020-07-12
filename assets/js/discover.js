f75855bba57c2ff49051a2780b8687eb29f6eab0
jess-bennett <jessica_bennett@gmx.com>
2020-07-12 09:19:29 +0000

    JS added to display selected letter in discover--question div

--- assets/js/discover.js
+++ assets/js/discover.js
@@ -2,3 +2,15 @@
 $(window).on('load', function() {
   $(".se-pre-con").fadeOut("slow");
 });
+
+/* On page load */
+$(document).ready(function() {
+  $("#discover--answer").css("display", "none");
+});
+
+$(".discover--letter--circle").click(function() {
+    let selectedLetter = $(this).text();
+    $("#discover--answer").css("display", "block");
+    $("#discover--letters").css("display", "none");
+    $("#discover--question").html(`How many things can you find that begin with the letter ${selectedLetter}?`);
+})

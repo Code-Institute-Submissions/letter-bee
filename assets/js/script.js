f1f8415ca216c943d8155a72c99417c52b5951dc
jess-bennett <jessica_bennett@gmx.com>
2020-07-23 18:18:09 +0000

    Single function to show/hide sections added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,36 @@
 $("#btn--to--about").click(function() {
     $("nav").css("visibility", "visible");
     $("footer").css("visibility", "visible");
-    $("#home").css("display", "none");
-    $("#about").css("display", "block");
+    showHideSections($("#home"),$("#about"));
 })
 
-$("#btn--to--play").click(function() {
-    $("#about").css("display", "none");
-    $("#play").css("display", "block");
-    navBarActive($("#span--about"),$("#span--play"));
+$(".btn--to").click(function() {
+    let targetActiveElementFull = $(this).prop('id');
+    let targetActiveElement = targetActiveElementFull.substring(9);
+    let currentActiveElement = $(".page--display").not(":hidden").prop("id");
+    showHideSections(`#${currentActiveElement}`,`#${targetActiveElement}`);
+    navBarActive($(`#span--${currentActiveElement}`),$(`#span--${targetActiveElement}`));
 })
+
+function showHideSections(sectionToHide,sectionToShow) {
+    $(sectionToHide).css("display", "none");
+    $(sectionToShow).css("display", "block");
+}
 
 /*
 * Show active link on navbar
 */
+
+$(".nav-link").click(function() {
+    let currentActiveElementFull = $(".nav-item.nav-link.active").prop('id');
+    let targetActiveElementFull = $(this).prop('id');
+    let currentActiveElement = currentActiveElementFull.substring(6);
+    let targetActiveElement = targetActiveElementFull.substring(6);
+    showHideSections(`#${currentActiveElement}`,`#${targetActiveElement}`);
+    navBarActive($(`#${currentActiveElementFull}`),$(`#${targetActiveElementFull}`));
+    navBarCollapse();
+})
+
 function navBarActive(inactiveElement, activeElement) {
     $(inactiveElement).removeClass("active");
     $(activeElement).addClass("active");
@@ -28,31 +45,3 @@ function navBarCollapse() {
     $(".navbar-collapse").removeClass("show");
     $(".animated-icon2").removeClass("open");
 }
-
-/* $("#span--about").click(function() {
-    let currentActiveElement = $(".nav-item.nav-link.active").attr('id');
-    navBarActive($(`#${currentActiveElement}`),$("#span--about"));
-    navBarCollapse();
-})
-$("#span--play").click(function() {
-    let currentActiveElement = $(".nav-item.nav-link.active").attr('id');
-    navBarActive($(`#${currentActiveElement}`),$("#span--play"));
-    navBarCollapse();
-})
-$("#span--learn").click(function() {
-    let currentActiveElement = $(".nav-item.nav-link.active").attr('id');
-    navBarActive($(`#${currentActiveElement}`),$("#span--learn"));
-    navBarCollapse();
-})
-$("#span--discover").click(function() {
-    let currentActiveElement = $(".nav-item.nav-link.active").attr('id');
-    navBarActive($(`#${currentActiveElement}`),$("#span--discover"));
-    navBarCollapse();
-}) */
-
-$(".nav-link").click(function() {
-    let currentActiveElement = $(".nav-item.nav-link.active").attr('id');
-    let targetActiveElement = $(this).attr('id');
-    navBarActive($(`#${currentActiveElement}`),$(`#${targetActiveElement}`));
-    navBarCollapse();
-})

18bea24219d2bea198f69aacb3ec2b80e9f1809a
jess-bennett <jessica_bennett@gmx.com>
2020-07-23 14:40:33 +0000

    Navbar functionality recreated in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,15 @@ $("#btn--to--about").click(function() {
 $("#btn--to--play").click(function() {
     $("#about").css("display", "none");
     $("#play").css("display", "block");
+    navBarActive($("#span--about"),$("#span--play"));
 })
+
+/*
+* Show active link on navbar
+*/
+function navBarActive(inactiveElement, activeElement) {
+    $(inactiveElement).removeClass("active");
+    $(activeElement).addClass("active");
+    $(inactiveElement).children("span").remove();
+    $(activeElement).append(` <span class="sr-only">(current)</span>`);
+}
